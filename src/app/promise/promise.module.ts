241101eb0423c9aec8a3791ed143dde71ca422e4
shindetejas11 <110617094+shindetejas11@users.noreply.github.com>
2022-11-10 08:17:44 +0530

    initial commit new

---
+++ src/app/promise/promise.module.ts
@@ -0,0 +1,18 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// // import { PromiseRoutingModule } from './promise-routing.module';
+// // import { PromisComponent } from './promis/promis.component';
+
+
+// @NgModule({
+//   declarations: [
+//     PromisComponent
+//   ],
+//   imports: [
+//     CommonModule,
+//     PromiseRoutingModule
+//   ],
+//   exports :[PromisComponent]
+// })
+// export class PromiseModule { }
